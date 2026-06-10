29d0b5a84d2fdbc9fd1dae8bc1cf9b8557bb908f
Wenry00 <henrym4321@gmail.com>
2026-06-09 23:50:42 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -38,6 +38,13 @@ function App() {
             <a href="#quote" className="button primary">Request a Free Quote <ArrowRight size={18} /></a>
             <a href="#systems" className="button secondary">View Systems</a>
           </div>
+          <div className="contact-info">
+  <a href="tel:4155470434">(415) 547-0434</a>
+
+  <a href="mailto:info@goldencrestwater.com">
+    info@goldencrestwater.com
+  </a>
+</div>
           <div className="proof-row">
             <span><CheckCircle2 size={17} /> Free water consultation</span>
             <span><CheckCircle2 size={17} /> Local Bay Area service</span>
